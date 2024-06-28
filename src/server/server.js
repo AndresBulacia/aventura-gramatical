46e8a78447f8fc597d215dd27afa1a9ce0d2666f
AndresBulacia <andresbulacia040@gmail.com>
2024-06-27 23:49:14 -0300

    servidor

--- src/server/server.js
+++ src/server/server.js
@@ -1,20 +1,16 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const app = express();
+require('dotenv').config();
 
-const DB_HOST = process.env.DB_HOST || 'localhost'
-const DB_USER = process.env.DB_USER || 'root'
-const DB_PASSWORD = process.env.DB_PASSWORD || 'Sanmartin3423!'
-const DB_NAME = process.env.DB_NAME || 'aventura_gramatical'
-const DB_PORT = process.env.DB_PORT || 3306
 
 // Conexión a MySQL
 const db = mysql.createConnection({
-    user: DB_USER,
-    password: DB_PASSWORD,
-    host: DB_HOST,
-    port: DB_PORT,
-    database: DB_NAME
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 // Guarda los puntos
