a97beb5c9fa4b680e79d8b8644031d4f8b93e289
AndresBulacia <andresbulacia040@gmail.com>
2024-06-30 01:08:44 -0300

    fix error con railway

--- src/server/server.js
+++ src/server/server.js
@@ -9,14 +9,20 @@ app.use(express.json()); // Asegúrate de que esta línea esté aquí para parse
 
 const PORT = process.env.PORT || 3306;
 
-// Conexión a MySQL sin SSL
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
+    ssl: {
+        // Path to SSL certificates if needed
+        ca: fs.readFileSync('/path/to/ca-cert.pem'),
+        key: fs.readFileSync('/path/to/client-key.pem'),
+        cert: fs.readFileSync('/path/to/client-cert.pem')
+    }
 });
+
 
 db.connect((err) => {
     if (err) {
